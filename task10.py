d73c66ce24ebc837c4e11a6db7b141d3056e3d22
Ozodbek Tolliyev <ozodbektolliyev179@gmail.com>
2025-12-25 16:52:39 +0500

    Add functionality to update fruit list by index

---
+++ task10.py
@@ -0,0 +1,8 @@
+mivalar = ["Olma", "Banan", "Uzum", "Anor"]
+
+index = int(input("Indeks kiriting (0 dan boshlanadi): "))
+name  = input("Yangi qiymatni kiriting: ")
+
+mivalar[index] = name
+
+print(mivalar)
